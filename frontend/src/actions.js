e283d04cdff68c05e5450b983ea6720445e55619
FAYCEL TOUILI <fayceltouili@gmail.com>
2019-09-18 16:46:17 -0700

    ready to deploy frontend

--- frontend/src/actions.js
+++ frontend/src/actions.js
@@ -13,7 +13,7 @@ import { ADD_POST,
 import axios from 'axios';
 
 
-const BASE_URL= 'http://localhost:5000';
+const BASE_URL= process.env.REACT_APP_BASE_URL || 'http://localhost:5000';
 
 function errorInAPI(errorMessage) {
   return {
