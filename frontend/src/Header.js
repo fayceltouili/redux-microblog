7145470b000a7a5df8edf06a63111b1da1e5c972
FAYCEL TOUILI <fayceltouili@gmail.com>
2019-09-19 00:23:24 -0700

    updated blog editor

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -9,8 +9,8 @@ class Header extends Component {
   }
   render() {
     return (
-      <div className="Header container">
-        <h1>{this.props.title}</h1>
+      <div className="Header">
+        <h1 style={{fontSize: '4em'}}>{this.props.title}</h1>
         <p>{this.props.description}</p>
         <Link to='/' className="HeaderLinks" style={{ textDecoration: 'none' }}> Blog</Link>
         <Link to='/new' className="HeaderLinks" style={{ textDecoration: 'none' }}>Add a new post</Link>
